236686ab80dfff83382c4a486485cca2ed81f2e7
rkcudjoe <=(none)>
2018-11-06 01:47:35 -0500

    calculateScore function works

--- scripts.js
+++ scripts.js
@@ -5,17 +5,17 @@ let question_four = "Do you have an Explainer Video on your home page?";
 let question_five = "Do you utilize separate landing pages for specific products and services?";
 let question_six = "Do you send automated follow emails to build the relationships?";
 let question_seven = "Do you have a Youtube channel?";
-let question_eight = "On Youtube channel do you create short but valuable 'Education Based videos to help your target market make a good buying decision?";
-let question_nine = "Are you a published author ? Meaning have you published a print book for credibility and authority in your marketplace?";
-let question_ten = "Do you send out broadcast emails,text messages,Tweets and Facebook posts about special deals or events?";
-let question_eleven = "Do you run limited time special promotions for customers only ? ";
+let question_eight = "On Youtube channel, do you create short but valuable 'Education Based' videos to help your target market make a good buying decision?";
+let question_nine = "Are you a published author? Meaning have you published a print book for credibility and authority in your marketplace?";
+let question_ten = "Do you send out broadcast emails, text messages, Tweets and Facebook posts about special deals or events?";
+let question_eleven = "Do you run limited time special promotions for customers only? ";
 let question_twelve= "Do you survey your customers to find out what they want most? ";
 let question_thirteen = "Do you offer upsells and cross sells after a customer has made an initial purchase? ";
 let question_fourteen = "Do you issue press releases on a regular basis to introduce new products, services and other newsworthy topics?";
 let question_fifteen = "Is your website mobile friendly and optimized?";
 let question_sixteen  = "Do you encourage good customers to leave online review on Yelp and Google?";
 let question_seventeen = "Do you partner with other local, non-competing businesses to cross promote each other?";
-let question_eighteen = "Do you have your business listed in the Top 15 local Directories ?Ex., Have you claimed your Google local listing? ";
+let question_eighteen = "Do you have your business listed in the Top 15 local Directories? Ex., Have you claimed your Google local listing? ";
 let question_ninteen = "Have you tested Google and/or Facebook ads to attract customers?";
 let question_twenty  = "Do you market to former customers to bring them back?";
 
@@ -55,18 +55,20 @@ function startQuiz() {
   	<div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
 	 </div>`;
 	 document.getElementById("content").innerHTML += `YES &nbsp; NO`
-	
+	 document.getElementById("content").innerHTML +=
+	 `
+	 <form name="quizForm" id="quizForm">
+	 </form>
+	 `
 	for (let quizIndex = 0; quizIndex < quizQuestions.length; quizIndex++) {
-		document.getElementById("content").innerHTML +=
+		document.getElementById("quizForm").innerHTML +=
 		`<br>
 		&nbsp;
-		<form class="questionForm form-check form-check-inline">
-		<input class="form-check-input" type="radio" name="#" value="1" aria-label="Checkbox for YES" required> &nbsp; &nbsp; &nbsp; 
-		<input class="form-check-input" type="radio" name="#" value="0" aria-label="Checkbox for NO" required>
-		</form>
+		<input type="radio" name="quizQuestion${quizIndex}" value="1" aria-label="Checkbox for YES" required> &nbsp; &nbsp; &nbsp; 
+		<input type="radio" name="quizQuestion${quizIndex}" value="0" aria-label="Checkbox for NO" required>
 		&nbsp;&nbsp;&nbsp;`
 
-		document.getElementById("content").innerHTML += `${quizQuestions[quizIndex].questionText}<br>`;
+		document.getElementById("quizForm").innerHTML += `${quizQuestions[quizIndex].questionText}<br>`;
 
 	}
 		document.getElementById("content").innerHTML += `<br><button type="button" class="btn btn-outline-info btn-lg" onclick="calculateScore()">Get My Score</button>`;
@@ -74,5 +76,16 @@ function startQuiz() {
 }
 
 function calculateScore() {
-	
-	} 
+	let yesScore = 0;
+	for (let quizIndex = 0; quizIndex < quizQuestions.length; quizIndex++) {
+		let quizValue = quizForm['quizQuestion'+quizIndex].value;
+ 		if(quizValue === "1"){
+ 			yesScore++;
+ 			console.log(yesScore);
+ 		} else {
+ 			console.log("create an array of TASK");
+ 		}
+ 	}
+ 	//return yesScore;
+	//let yesScore = document.getElementsByTagName("input[='quizQuestion0']").value;
+} 
